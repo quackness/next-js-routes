b7cba6a0c237d33bcf1662e1e89bf742d3678949
Karolina Redden <dubajkaro@gmail.com>
2024-02-29 21:03:33 -0400

    add dynamic route

---
+++ src/api/todos/[id]/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+
+const DATA = "https://jsonplaceholder.typicode.com/todos";
+const API_KEY: string = process.env.DATA_API_KEY as string;
+console.log(API_KEY)
+
+export async function GET(request: Request) {
+  const id = request.url.slice(request.url.lastIndexOf("/") + 1);
+  const res = await fetch(`${DATA}/${id}`);
+  const todo: Todo = await res.json();
+  if (!todo.id) return NextResponse.json({ message: "Inavlid" })
+  return NextResponse.json(todo);
+}
